adce5827f3fd341ea5b98d6feb0cf26f1bb33753
Rajendra Nayak <rnayak@ti.com>
2014-02-26 14:43:13 +0530

    ARM: DRA7: Add support for soc_is_dra74x() and soc_is_dra72x() varients
    Use the corresponding compatibles to identify the devices.
    
    Signed-off-by: Rajendra Nayak <rnayak@ti.com>
    
    Conflicts:
    
    	arch/arm/mach-omap2/soc.h
    [subramaniam.ca@ti.com: ported to kernel 3.8]
    Signed-off-by: Subramaniam Chanderashekarapuram <subramaniam.ca@ti.com>
    
    Change-Id: I9abe4f83d83033dfbc734bd9d289b65177b53cb4

--- arch/arm/mach-omap2/soc.h
+++ arch/arm/mach-omap2/soc.h
@@ -248,7 +248,8 @@ IS_AM_SUBCLASS(335x, 0x335)
 #define soc_is_omap54xx()		0
 #define soc_is_omap543x()		0
 #define soc_is_dra7xx()			0
-#define soc_is_dra75x()			0
+#define soc_is_dra74x()			0
+#define soc_is_dra72x()			0
 
 #if defined(MULTI_OMAP2)
 # if defined(CONFIG_ARCH_OMAP2)
@@ -390,9 +391,11 @@ IS_OMAP_TYPE(3430, 0x3430)
 
 # if defined(CONFIG_SOC_DRA7XX)
 # undef soc_is_dra7xx
-# undef soc_is_dra75x
+#undef soc_is_dra74x
+#undef soc_is_dra72x
 # define soc_is_dra7xx()		is_dra7xx()
-# define soc_is_dra75x()		is_dra75x()
+# define soc_is_dra74x()		is_dra74x()
+# define soc_is_dra72x()		is_dra72x()
 #endif
 
 /* Various silicon revisions for omap2 */
